a6371026d27095197742ba6d946d8a4de7d52457
miliegoat <sotarksonosu@gmail.com>
2026-06-08 17:11:02 +0200

    asd

--- js/revealing.js
+++ js/revealing.js
@@ -1,10 +1,7 @@
 export function initRevealAnimations() {
-  var els = document.querySelectorAll(".reveal");
-  if (!els.length) return;
-
   function check() {
     var h = window.innerHeight;
-    els.forEach(function (el) {
+    document.querySelectorAll(".reveal").forEach(function (el) {
       var r = el.getBoundingClientRect();
       if (r.top < h - 40 && r.bottom > 40) {
         el.classList.add("revealed");
